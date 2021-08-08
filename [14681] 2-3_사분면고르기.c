360f906169b9c9f02f73dafd9a98b868a9685784
jisubaek <bbbjisu@gmail.com>
2021-08-09 01:45:05 +0900

    up

---
+++ [14681] 2-3_사분면고르기.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+	int x, y;
+	scanf_s("%d", &x);
+	scanf_s("%d", &y);
+
+	if (x > 0) {
+		if (y > 0)
+			printf("1");
+		else
+			printf("4");
+	}
+	else {
+		if (y > 0)
+			printf("2");
+		else
+			printf("3");
+	}
+	
+	return 0;
+
+}
